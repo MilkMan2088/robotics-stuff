3acc8cd9b13b89611d4221e0234ef8553b9d5e96
Ramil <ramil@Ardos-MacBook-Pro.local>
2024-12-19 19:41:15 -0700

    first commit

---
+++ include/subsystems.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "api.h"
+
+// Your mosszzztors, sensors, etc. should go here.  Below are examples
+
+inline pros::Motor mouth(18);
+inline pros::Motor arm(20);
+// inline pros::Motor intake(1);
+// inline pros::adi::DigitalIn limit_switch('A');
